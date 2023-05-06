49ba4ba100b54fd9e1aec528e1f0035fce2207b7
Amadimm <amadeuszrolecki@gmail.com>
2023-05-06 14:04:28 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryArray = document.querySelector(`.gallery`);
+
+const newImages = galleryItems
+  .map(
+    image =>
+      `<li>
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+   
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(' ');
+
+galleryArray.style.listStyleType = 'none';
+
+galleryArray.insertAdjacentHTML('beforeend', newImages);
+
+console.log(galleryItems);
+
+// kliknięcie w obrazki, otwieranie i zamykanie okna modalnego oraz przewijaniem obrazków za pomocą klawiatury.
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
